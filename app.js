704368ec092b6c5d74d1f050357dbbc55c1049f4
gefangenimnetz <kontakt@florian-kutschera.de>
2011-08-08 07:47:35 +0000

    added views

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ var express = require('express');
 
 var app = express.createServer();
 
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Index
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('index.jade', {layout: false});
+});
+
+// Alle Listen (für den jeweiligen User)
+app.get('/lists/', function(req, res){
+  res.render('lists.jade'); 
 });
 
 app.listen(13337);
